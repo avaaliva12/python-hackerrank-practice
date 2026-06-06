727d7082a9001d6b009c66154d5b119bbd972f0a
avaaliva12 <avaaliva12@gmail.com>
2026-06-06 19:23:40 +0530

    Done

---
+++ Basics/Largest_of_Two_Numbers.py
@@ -0,0 +1,7 @@
+#Find the largest of two numbers
+N1 = int(input("Enter 1st Number: "))
+N2 = int(input("Enter 2nd Number: "))
+if N1 > N2:
+    print(N1, " is largest")
+else:
+    print(N2, " is largest")
