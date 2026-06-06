971c19bb6acaf7017409f6bffb676bc794a3e38c
avaaliva12 <avaaliva12@gmail.com>
2026-06-06 20:19:04 +0530

    Done

---
+++ Operators/Quotient_and_Remainder.py
@@ -0,0 +1,6 @@
+#Find quotient and remainder
+dividend = int(input("Enter dividend: "))
+divisor = int(input("Enter divisor: "))
+quotient = dividend / divisor
+remainder = dividend % divisor
+print("quotient: ", quotient, "remainder: ", remainder)
