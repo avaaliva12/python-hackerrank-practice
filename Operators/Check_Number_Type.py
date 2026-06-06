c1a68a7d8938cc84b5557e20a368fb85545efc57
avaaliva12 <avaaliva12@gmail.com>
2026-06-06 20:46:28 +0530

    Done

---
+++ Operators/Check_Number_Type.py
@@ -0,0 +1,8 @@
+#Find whether a number is positive, negative, or zero
+n = int(input("Enter any Number: " ))
+if n == 0:
+    print(n , " is zero.")
+elif n > 0:
+    print(n, " is positive")
+else:
+    print(n, " is negative")
