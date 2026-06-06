5c09328eb04923d1a56d58576c60dad38e421724
avaaliva12 <avaaliva12@gmail.com>
2026-06-06 19:33:13 +0530

    Done

---
+++ Basics/Simple_Interest.py
@@ -0,0 +1,6 @@
+#Calculate simple interest
+p = float(input("Enter Principal Amount: "))
+r = float(input("Enter Rate Of Interest: "))
+t = float(input("Enter Time Period: "))
+si = (p*r*t)/100
+print("Simple Interest: ", si)
