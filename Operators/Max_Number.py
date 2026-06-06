09b46cdc4fd07ff2aab905db39073e26f4da1bde
avaaliva12 <avaaliva12@gmail.com>
2026-06-06 21:08:52 +0530

    Done

---
+++ Operators/Max_Number.py
@@ -0,0 +1,7 @@
+#Find maximum using ternary operator
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+maximum = a if a > b else b
+
+print("Maximum number is:", maximum)
