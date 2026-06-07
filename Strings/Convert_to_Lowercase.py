57df5e129518b474c8ca348376c964a6cc6c5258
avaaliva12 <avaaliva12@gmail.com>
2026-06-07 21:10:22 +0530

    Done

---
+++ Strings/Convert_to_Lowercase.py
@@ -0,0 +1,4 @@
+#Convert to lowercase
+str = input("Enter a word: ")
+str = str.lower()
+print(str)
---
+++ Strings/Convert_to_Uppercase.py
@@ -0,0 +1,4 @@
+#Convert to uppercase
+str = input("Enter a word: ")
+str = str.upper()
+print(str)
